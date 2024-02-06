1ab3b9041c71d01527cf2455aa1ff227bba48bd6
guillermo <you@example.com>
2024-02-06 18:59:49 -0300

    Actualizada convencion de comandos en .sh

--- scripts/start_docker_graphics.sh
+++ scripts/start_docker_graphics.sh
@@ -5,9 +5,9 @@ docker volume create --driver local --opt type=none --opt device="$(pwd)/../dist
 xhost +local:root &&\
 docker run --rm -it --net=host --privileged --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"  \
-  --volume="`pwd`/../docker-home:/home/duser:rw" \
-  --volume="`pwd`/../carrolac-ros2ws:/home/duser/carrolac-ros2ws:rw" \
-  --volume="`pwd`/../venvs:/home/duser/venvs:rw" \
+  --volume="$(pwd)/../docker-home:/home/duser:rw" \
+  --volume="$(pwd)/../carrolac-ros2ws:/home/duser/carrolac-ros2ws:rw" \
+  --volume="$(pwd)/../venvs:/home/duser/venvs:rw" \
   --volume="/etc/localtime:/etc/localtime:ro" \
   --volume="docker-site:/opt/ros/humble/lib/python3.10/site-packages" \
   --volume="docker-dist:/opt/ros/humble/local/lib/python3.10/dist-packages" \
