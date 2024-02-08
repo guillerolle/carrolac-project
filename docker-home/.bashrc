2d06bc64ae38dc066c4f28688e4e6d83dc42c75e
guillermo <you@example.com>
2024-02-08 13:56:21 -0300

    Solucionado falla de KeyError: 'launch' al compilar y sourcear workspace

--- docker-home/.bashrc
+++ docker-home/.bashrc
@@ -3,6 +3,6 @@ echo SOURCEANDO .BASHRC...
 source ~/venvs/$(hostname)/$(id --user --name)-python310.venv/bin/activate
 source /opt/ros/humble/setup.bash;
 cd ~/carrolac-ros2ws;
-source install/setup.bash;
+source install/local_setup.bash;
 export ROS_LOG_DIR=~/carrolac-ros2ws/log
 export PYTHONWARNINGS='ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources'
--- scripts/build_workspace.sh
+++ scripts/build_workspace.sh
@@ -2,7 +2,13 @@
 # Este archivo corre la calibracion en un docker
 
 xhost +local:root &&\
-docker run --rm -it --net=host --privileged --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="`pwd`/../docker-home:/home/duser:rw" --volume="/etc/localtime:/etc/localtime:ro" ros2:humble_with_gazebo bash -c "source ~/.bashrc; colcon build" &&\
+docker run --rm -it --net=host --privileged --env="DISPLAY" --env="QT_X11_NO_MITSHM=1"\
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="$(pwd)/../docker-home:/home/duser:rw" \
+  --volume="$(pwd)/../carrolac-ros2ws:/home/duser/carrolac-ros2ws:rw" \
+  --volume="$(pwd)/../venvs:/home/duser/venvs:rw" \
+  --volume="/etc/localtime:/etc/localtime:ro" \
+   ros2:humble_with_gazebo bash -c "source ~/.bashrc; colcon build" &&\
 
 # Detiene el contenedor
 xhost -local:root
