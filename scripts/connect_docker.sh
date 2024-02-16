aeda709a9e7374ca4a2ae1fb6b51f7e8288f662c
guillermo <guille.rolle@hotmail.com>
2024-02-16 13:03:20 -0300

    Fixed syntax

--- scripts/connect_docker.sh
+++ scripts/connect_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-docker exec -it `docker ps | awk '/ros2:humble_with_gazebo/ { print $1 }'` bash
+docker exec -it "$(docker ps | awk '/ros2:humble_with_gazebo/ { print $1 }')" bash
